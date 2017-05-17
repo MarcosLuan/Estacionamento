9009a4626c33ce0048968d1d5961c9222110ca4e
MarcosLuan <mluank@gmail.com>
2017-05-17 20:45:03 -0300

    Arquivos acrescentados

---
+++ Trabalho/src/java/br/senai/sc/entity/Vagas.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.entity;
+
+/**
+ *
+ * @author Marcos_Laís
+ */
+public class Vagas {
+    private String codigo;
+    private String fileira;
+    private String posicao;
+    private String status;
+    private String disp;
+
+    public Vagas() {
+    }
+
+    public Vagas(String codigo, String fileira, String posicao, String status, String disp) {
+        this.codigo = codigo;
+        this.fileira = fileira;
+        this.posicao = posicao;
+        this.status = status;
+        this.disp = disp;
+    }
+
+   
+    
+    
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getFileira() {
+        return fileira;
+    }
+
+    public void setFileira(String fileira) {
+        this.fileira = fileira;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDisp() {
+        return disp;
+    }
+
+    public void setDisp(String disp) {
+        this.disp = disp;
+    }
+
+    
+}
---
+++ Trabalho/src/java/br/senai/sc/servlet/VagaServlet.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.servlet;
+
+import br.senai.sc.DAO.VagaDAO;
+import br.senai.sc.entity.Vagas;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Marcos_Laís
+ */
+@WebServlet(name = "VagaServlet", urlPatterns = {"/VagaServlet"})
+public class VagaServlet extends HttpServlet {
+
+    
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    String codigo = request.getParameter("codigo");
+    String fileira = request.getParameter("fileira");
+    String posicao = request.getParameter("posicao");
+    String status = request.getParameter("status");
+    String disp = request.getParameter("disp");
+    
+    Vagas v = new Vagas(codigo, fileira, posicao, status, disp);
+    
+    VagaDAO vdao = new VagaDAO();
+    
+    try {
+        vdao.insert(v);
+        request.getRequestDispatcher("Cadastradas.jsp").forward(request, response);
+        
+    } catch (Exception ex){
+        request.setAttribute("erro", ex.getMessage());
+        request.getRequestDispatcher("erro.jsp").forward(request, response);
+        
+    }
+    
+    }
+
+}
