554fc49b266be11bf21dc1d119643b9c453cfda2
Erik Sidnei Corrêa <erik562@hotmail.com>
2017-05-24 21:49:31 -0300

    .

--- Trabalho/src/java/br/senai/sc/servlet/UsuarioServlet.java
+++ Trabalho/src/java/br/senai/sc/servlet/UsuarioServlet.java
@@ -5,8 +5,12 @@
  */
 package br.senai.sc.servlet;
 
+import br.senai.sc.DAO.UsuarioDAO;
+import br.senai.sc.entity.Usuario;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,7 +27,24 @@ public class UsuarioServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        request.
+        String placa = request.getParameter("placa");
+        String nome = request.getParameter("nome");
+        
+        Usuario usuario = new Usuario(placa,nome);
+        UsuarioDAO uDAO = new UsuarioDAO();
+        
+        try{
+            uDAO.insert(usuario);
+            
+        } catch (Exception ex) {
+            Logger.getLogger(UsuarioServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        
+        
+        
     }
 
     
